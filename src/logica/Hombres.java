362fcbb25c98be7b606a8209d8839e38f34ba05f
David-F-Canon-C <df.canon@uniandes.edu.co>
2026-09-08 09:16:03 -0500

    Intento de implementación de Principal

--- src/logica/Hombres.java
+++ src/logica/Hombres.java
@@ -1,5 +1,20 @@
 package logica;
 
-public class Hombres {
-
+public class Hombres extends Persona implements TMB{
+	
+	public Hombres(String nombre, double peso_kg, int altura_cm, int edad){
+		super(nombre, peso_kg, altura_cm, edad);
+	}
+	
+	
+	@Override
+	public double tasa_met() {
+		if(this.peso_kg >= 60 && this.peso_kg <= 110 && this.altura_cm >= 160 && this.altura_cm <= 195 && this.edad > 15) {
+			return 88.362 + (13.397 * this.peso_kg) + (4.799 * this.altura_cm) - (5.677 * this.edad);
+		}
+		else {
+			return -1;
+		}
+	}
+	
 }
--- src/presentacion/Principal.java
+++ src/presentacion/Principal.java
@@ -1,10 +1,47 @@
 package presentacion;
 
-public class Principal {
+import logica.TMB;
+import logica.Persona;
+import logica.Hombres;
+import logica.Mujeres;
+
+public class Principal{
+	
+	public Principal() {
+		Persona p;
+		TMB tasa;
+		
+		p = new Hombres("Paco", 80, 170, 18);
+		
+		System.out.println(p.getNombre());
+		System.out.println(p.getEdad());
+		System.out.println(p.getPeso());
+		System.out.println(p.getAltura());
+		
+		Hombres c = p;
+		
+		tasa = System.out.println(c.tasa_met());
+		
+		p = new Mujeres("Luisa", 60, 150, 19);
+		System.out.println(p.getNombre());
+		System.out.println(p.getEdad());
+		System.out.println(p.getPeso());
+		System.out.println(p.getAltura());
+		
+		Mujeres T = p;
+		
+		tasa = System.out.println(c.tasa_met());
+		
+		
+		
+	}
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		new Principal();
+		
 	}
 
 }
