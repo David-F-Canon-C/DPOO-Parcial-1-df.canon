c38511bfe24b1668789d940f293ed8cd361d48e6
David-F-Canon-C <df.canon@uniandes.edu.co>
2026-09-08 09:03:27 -0500

    Implementación de clases hombres y mujeres

--- src/logica/Mujeres.java
+++ src/logica/Mujeres.java
@@ -1,5 +1,19 @@
 package logica;
 
-public class Mujeres {
-
+public class Mujeres extends Persona implements TMB{
+	
+	public Mujeres(String nombre, double peso_kg, int altura_cm, int edad){
+		super(nombre, peso_kg, altura_cm, edad);
+	}
+	
+	
+	@Override
+	public double tasa_met() {
+		if(this.peso_kg >= 40 && this.peso_kg <= 80 && this.altura_cm >= 140 && this.altura_cm <= 180 && this.edad > 15) {
+			return 447.593 + (9.247 * this.peso_kg) + (3.098 * this.altura_cm) - (4.33 * this.edad);
+		}
+		else {
+			return -1;
+		}
+	}
 }
